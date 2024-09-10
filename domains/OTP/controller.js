0ae180521c4ffe548dadfae7f583fdcf606a86a9
manlikexavy <chibokaxavier@gmail.com>
2024-09-10 23:15:49 +0100

    undoing the changes made for the OTP

--- domains/OTP/controller.js
+++
@@ -1,5 +0,0 @@
-const sendOTP = async ({ email, subject, message, duration = 1 }) => {
-    try {
-    } catch (error) {}
-  };
-  
--- domains/OTP/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const OTPSchema = new Schema({
-  email: { type: string, unique: true },
-  otp: string,
-  createdAt: Date,
-  expiresAt: Date,
-});
