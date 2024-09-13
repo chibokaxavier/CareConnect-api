f2dff825587efa67f2f0fe3630fc58050fecb086
manlikexavy <chibokaxavier@gmail.com>
2024-09-13 19:20:02 +0100

    created the fn for the stripe payment, added the schema for it

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -36,31 +36,6 @@ router.post("/register", async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 });
-// router.post("/register", async (req, res) => {
-//   try {
-//     let { email, password, name, role, photo, gender } = req.body;
-//     name = name.trim();
-//     email = email.trim();
-//     password = password.trim();
-//     if (!(name && email && password)) {
-//       throw Error("Empty input fields!");
-//     } else if (!/^[a-zA-Z]*$/.test(name)) {
-//       throw Error("Invalid name entered!");
-//     } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-//       throw Error("Invalid email entered!");
-//     } else if (password.length < 8) {
-//       throw Error("Password should be at least 8 characters long");
-//     } else {
-//       const newUser = await register(req, res);
-//       const { password: hashedPassword, ...user } = newUser._doc;
-//       res
-//         .status(200)
-//         .json({ message: "User created successfully", data: user });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
 
 router.post("/login", async (req, res) => {
   try {
